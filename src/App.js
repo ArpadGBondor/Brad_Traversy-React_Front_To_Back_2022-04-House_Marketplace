7cafeffa4a90672350b07d693e15302695269d64
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2022-06-24 23:32:09 +0100

    Section 13 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
+
 function App() {
     return (
-        <div className="App">
-            <h1>House Marketplace</h1>
-        </div>
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Explore />} />
+                    <Route path="/offer" element={<Offers />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/forgotpassword" element={<ForgotPassword />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/*" element={<NotFound />} />
+                </Routes>
+                <Navbar />
+            </Router>
+        </>
     );
 }
 
